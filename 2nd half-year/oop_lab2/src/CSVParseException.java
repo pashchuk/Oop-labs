246891bbb2ae0e0d7d0dd4dd31e82f3df33885d3
MAMAxropela <mamaxropela@gmail.com>
2014-03-20 20:12:02 +0200

    add new class

---
+++ 2nd half-year/oop_lab2/src/CSVParseException.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Eduard on 3/20/2014.
+ */
+public class CSVParseException {
+
+}
---
+++ 2nd half-year/oop_lab2/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Eduard
+ * Date: 3/20/14
+ * Time: 12:25 AM
+ * To change this template use File | Settings | File Templates.
+ */
+import java.io.*;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        }
+    }
+}
