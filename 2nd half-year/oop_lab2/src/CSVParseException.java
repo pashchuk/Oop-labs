28fadeb19bb16ed0de567af6548319da8eabe08f
MAMAxropela <mamaxropela@gmail.com>
2014-03-21 02:22:35 +0200

    add error enum

--- 2nd half-year/oop_lab2/src/CSVParseException.java
+++ 2nd half-year/oop_lab2/src/CSVParseException.java
@@ -1,6 +1,31 @@
+import java.io.IOException;
+
 /**
  * Created by Eduard on 3/20/2014.
  */
-public class CSVParseException {
+public class CSVParseException extends IOException {
+        private String message;
+    public CSVParseException(CSVParseErrors error){
+        switch (error){
+            case invalidData:
+                message = "CSV file have some incorrect data";
+                break;
+            case isNotCSVFile:
+                message = "The current file is not a .csv file";
+                break;
+            case mustHaveMoreData:
+                message = "This file must have a more data";
+                break;
+            default:
+                break;
+        }
+    }
+    public CSVParseException(String message){
+        this.message = message;
+    }
 
+    @Override
+    public String getMessage() {
+        return message;
+    }
 }
--- 2nd half-year/oop_lab2/src/Main.java
+++ 2nd half-year/oop_lab2/src/Main.java
@@ -9,7 +9,7 @@ import java.io.*;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
-        
+
         }
     }
 }
