dea4e0dab79db028b0bc1d709ced8b5d5a3343ce
Eduard <mamaxropela@gmail.com>
2014-06-08 17:54:31 +0300

    delete xml tracker

--- 2nd half-year/oop_lab3/src/Sector.java
+++ 2nd half-year/oop_lab3/src/Sector.java
@@ -1,5 +1,13 @@
+import java.awt.*;
+
 /**
  * Created by mamaxropela on 6/8/14.
  */
 public class Sector {
+    private Color color;
+    private int value;
+    public Sector(int value, Color color){
+        this.value = value;
+        this.color = color;
+    }
 }
