84d5bd00a036e77709c84627464e7b461f182b5f
Eduard <mamaxropela@gmail.com>
2014-06-09 14:24:06 +0300

    fix bug with paint

--- 2nd half-year/oop_lab3/src/DiagramDrawer.java
+++ 2nd half-year/oop_lab3/src/DiagramDrawer.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
  * Created by mamaxropela on 5/6/14.
  */
 public class DiagramDrawer extends JComponent {
-    private ArrayList<Sector[]> sectors;
+    private ArrayList<Sector[]> sectors = null;
     private int[][] data;
     private final Color[] colors = {
             Color.green,Color.blue,
@@ -19,22 +19,24 @@ public class DiagramDrawer extends JComponent {
 
     private CSVProcessor processor;
     private JTable table;
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g);
-        g.setColor(Color.BLUE);
-        g.fillArc(10,10,100,100,0,50);
-        //draw(g);
-    }
+    boolean b = false;
+//    @Override
+//    public void paint(Graphics g) {
+////        super.paint(g);
+//        g.setColor(Color.BLUE);
+//        g.fillArc(10,10,100,100,0,50);
+////        draw(g);
+//    }
+
 
     @Override
-    protected void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.BLUE);
-        g.fillArc(10,10,100,100,0,50);
+        //if (b) g.fillRect(10, 10, 100, 100);
+//        g.fillArc(10,10,100,100,0,50);
         draw(g);
     }
-    pai
 
     public DiagramDrawer(CSVProcessor proc){
         this.processor = proc;
@@ -52,24 +54,26 @@ public class DiagramDrawer extends JComponent {
         String[][] csvTable = processor.getData();
         data = new int[csvTable.length][csvTable[0].length];
         for(int i = 0; i < csvTable.length; i++){
-            sectors.add(new Sector[csvTable[0].length]);
+            Sector[] local = new Sector[csvTable[0].length];
             for(int j = 0; j < csvTable[0].length; j++)
-                sectors.get(i)[j] = new Sector(Integer.parseInt(csvTable[i][j]),getNewColor());
+                local[j] = new Sector(Integer.parseInt(csvTable[i][j]),getNewColor());
                 //data[i][j] = Integer.parseInt(csvTable[i][j]);
+            sectors.add(local);
         }
+        b = true;
     }
     private void draw(Graphics g){
-        int x = 10, y = 10, width = 300, height = 300,
+        int x = 10, y = 10, width = 200, height = 200,
                 startAngle = 0, endAngle = 0, maxValue = 0;
         Sector currSector;
         for(int j = 0; j < sectors.get(0).length; j++)
             maxValue += sectors.get(0)[j].getValue();
         for(int i = 0; i < sectors.get(0).length; i++){
             currSector = sectors.get(0)[i];
-            endAngle = (int)(currSector.getValue()*10/maxValue/36) + startAngle;
+            endAngle = (int) ((double) currSector.getValue()/maxValue*360);
             g.setColor(currSector.getColor());
             g.fillArc(x,y,width,height,startAngle,endAngle);
-            startAngle = endAngle;
+            startAngle += endAngle;
         }
     }
     private Color getNewColor(){
--- 2nd half-year/oop_lab3/src/Main.java
+++ 2nd half-year/oop_lab3/src/Main.java
@@ -5,6 +5,9 @@
  * Time: 12:25 AM
  * To change this template use File | Settings | File Templates.
  */
+import com.sun.corba.se.impl.orbutil.graph.Graph;
+
+import java.awt.*;
 import java.io.*;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -14,14 +17,20 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         JFrame frame = new JFrame("MainFrame");
+        Graphics a = frame.getGraphics();
         CSVProcessor processor = new CSVProcessor("asd.csv");
         DiagramDrawer drawer = new DiagramDrawer(processor);
-        frame.setBounds(100,100,600,600);
+        drawer.setPreferredSize(new Dimension(500,500));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setBounds(100, 100, 600, 600);
         frame.setResizable(false);
         frame.setLayout(null);
-        frame.getContentPane().add(drawer);
-        //frame.getContentPane().add(drawer.getTable());
+        drawer.setSize(300, 300);
+        drawer.setLocation(300, 300);
+        frame.add(drawer);
+        frame.getContentPane().add(drawer.getTable());
+        frame.validate();
+        Graphics b = frame.getGraphics();
         frame.setVisible(true);
-
     }
 }
